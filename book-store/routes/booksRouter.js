296929d7d955489fd30f94a3964fc63943e4b583
Jacob Kidd <kiddj1@students.westerntc.edu>
2023-06-08 14:57:38 -0500

    updated

--- book-store/routes/booksRouter.js
+++ book-store/routes/booksRouter.js
@@ -20,19 +20,47 @@ router.get('/', (req, res) => {
     res.json(books); //required
 });
 
-
+// GET localhost:3000/books/search?title={title}
 router.get('/search', (req, res) => {
-    const productName = req.query.name;
-    const requestProduct = books.find(prod => prod.name === productName);
-    if(requestProduct){
-        // we found it 
-        res.json(requestProduct);
+    //get the title from query params
+    //search the array of books for a book with the title given
+    const bookName = req.query.title;
+    //console.log(req.query);
+    const requestbook = books.find(searchBook => searchBook.title === bookName);
+    //console.log(requestbook);
+    if(requestbook){
+        // we found a book, return it
+        res.json(requestbook);
     }
     else{
-        // we didnt find it
-        res.status(404).json({error: "Product not found"});
+        // we didnt find it so we return a status of 404
+        res.status(404).json({error: "book wasn't found"});
     }
 });
 
+//POST localhost:3000/books
+router.post('/', (req, res) => {
+    const newBook = req.body;
+    //create id for the new book. Make if the length of the books array + 1
+
+    newBook.id = books.length + 1;
+    
+    //add the new book to the books array
+    books.push(newBook);
+    //write out the books array to the json file (out "database")
+    fs.writeFile(bookFile, JSON.stringify(books), (err) => {
+        if(err){
+            console.log("Error writing file: ", err);
+        }
+        else{
+            console.log("Successfully wrote file");
+        }
+    });
+    //return a 201 status code 
+
+    res.status(201).json({success: true, message: "Book created successfully"});
+
+
+});
 
 module.exports = router;
